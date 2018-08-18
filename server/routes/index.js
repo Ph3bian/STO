709cec50078442804138d7132e43ecc927e9a49d
Phebian Chukwurah <phebianchukwurah@gmail.com>
2018-08-18 23:48:23 +0100

    add an answer completed

--- server/routes/index.js
+++ server/routes/index.js
@@ -33,9 +33,55 @@ router.get('/questions/:id', (req, res) => {
     return res.status(404).json({
         status: 'fail',
         data: {
-          message: 'Question not found.',
+            message: 'Question not found.',
         },
-      });
+    });
 });
+
+router.post('/questions', (req, res) => {
+    // TODO: check if the data required was provided.
+    const { title, description, userId } = req.body;
+
+    const question = {
+        id: parseInt((Math.random() * 10000).toFixed(), 10),
+        title,
+        description,
+        userId,
+        repliesCount: 0,
+        created_at: new Date(),
+    };
+
+    questions.push(question);
+
+    return res.json({
+        status: 'success',
+        data: {
+            question,
+        },
+    });
+})
+
+router.post('/questions/:id/answers', (req, res) => {
+    // TODO: check if the data required is provided
+    const { title, description, userId } = req.body;
+
+    const question = {
+        id: parseInt((Math.random() * 10000).toFixed(), 10),
+        title,
+        description,
+        userId,
+        repliesCount: 0,
+        created_at: new Date(),
+    };
+
+    questions.push(question);
+
+    return res.json({
+        status: 'success',
+        data: {
+            question,
+        },
+    });
+})
 
 export default router;
